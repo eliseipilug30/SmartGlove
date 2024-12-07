2a35d6212712f3dbc0640dac0656ecdd0c06ea45
volcov_s <volcovsabina@gmail.com>
2024-12-07 22:55:02 +0200

    Got it now

--- backend/get-db-data.js
+++ backend/get-db-data.js
@@ -9,8 +9,8 @@ const pool = new Pool({
 
 const insertReading = async (temp, humidity, fire, light, co2) => {
     try {
-        const query = 'INSERT INTO local () VALUES ($1, $2, $3) RETURNING *';
-        const values = [name, address, contact_number];
+        const query = 'INSERT INTO local (temp, humidity, fire, light, co2) VALUES ($1, $2, $3) RETURNING *';
+        const values = [temp, humidity, fire, light, co2];
 
         const result = await pool.query(query, values);
         return result.rows[0];  // Return the newly inserted row
