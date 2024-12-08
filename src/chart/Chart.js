6c2b34592045308963fec5cb5394c4a2ab200587
volcov_s <volcovsabina@gmail.com>
2024-12-08 10:13:48 +0200

    periodic fetches

--- src/chart/Chart.js
+++ src/chart/Chart.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { Chart, CategoryScale, LinearScale, LineElement, Title, Tooltip, Legend, PointElement } from 'chart.js';
 import { LineController } from 'chart.js';
-import './styles/chart.css'
+import './styles/chart.css';
 
 Chart.register(CategoryScale, LinearScale, LineElement, Title, Tooltip, Legend, PointElement, LineController);
 
@@ -43,44 +43,53 @@ function ChartComponent() {
     });
 
     useEffect(() => {
-        fetch('http://localhost:3001/avg')
-            .then(response => response.json())
-            .then(data => {
-                setChartData({
-                    labels: data.batch_number,
-                    datasets: [
-                        {
-                            label: 'Average Heartrate',
-                            data: data.avg_heartrate,
-                            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                            borderColor: 'rgba(75, 192, 192, 1)',
-                            borderWidth: 1,
-                        },
-                        {
-                            label: 'Average Body Temperature',
-                            data: data.avg_body_temperature,
-                            backgroundColor: 'rgba(255, 159, 64, 0.2)',
-                            borderColor: 'rgba(255, 159, 64, 1)',
-                            borderWidth: 1,
-                        },
-                        {
-                            label: 'Average O2 Level',
-                            data: data.avg_o2level,
-                            backgroundColor: 'rgba(153, 102, 255, 0.2)',
-                            borderColor: 'rgba(153, 102, 255, 1)',
-                            borderWidth: 1,
-                        },
-                        {
-                            label: 'Average Stress',
-                            data: data.avg_stress,
-                            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                            borderColor: 'rgba(255, 99, 132, 1)',
-                            borderWidth: 1,
-                        },
-                    ],
-                });
-            })
-            .catch(error => console.error('Error fetching chart data:', error));
+        let intervalId;
+
+        const fetchData = () => {
+            fetch('http://localhost:3001/avg')
+                .then(response => response.json())
+                .then(data => {
+                    setChartData({
+                        labels: data.batch_number,
+                        datasets: [
+                            {
+                                label: 'Average Heartrate',
+                                data: data.avg_heartrate,
+                                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                                borderColor: 'rgba(75, 192, 192, 1)',
+                                borderWidth: 1,
+                            },
+                            {
+                                label: 'Average Body Temperature',
+                                data: data.avg_body_temperature,
+                                backgroundColor: 'rgba(255, 159, 64, 0.2)',
+                                borderColor: 'rgba(255, 159, 64, 1)',
+                                borderWidth: 1,
+                            },
+                            {
+                                label: 'Average O2 Level',
+                                data: data.avg_o2level,
+                                backgroundColor: 'rgba(153, 102, 255, 0.2)',
+                                borderColor: 'rgba(153, 102, 255, 1)',
+                                borderWidth: 1,
+                            },
+                            {
+                                label: 'Average Stress',
+                                data: data.avg_stress,
+                                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                                borderColor: 'rgba(255, 99, 132, 1)',
+                                borderWidth: 1,
+                            },
+                        ],
+                    });
+                })
+                .catch(error => console.error('Error fetching chart data:', error));
+        };
+
+        fetchData();
+        intervalId = setInterval(fetchData, 5000);
+
+        return () => clearInterval(intervalId);
     }, []);
 
     useEffect(() => {
